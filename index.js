8277eef2781503b5ce81038af86889bcb72e987a
nueldona <segunaiyedona@gmail.com>
2025-03-13 22:07:37 +0100

    login page

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 const multer = require("multer");
 
-
 const app = express();
 const PORT = 3000;
 
@@ -15,17 +14,38 @@ app.use(express.static("public"));
 const storage = multer.diskStorage({
   destination: "./uploads/",
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
   },
 });
 const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "Home", data: "Welcome to the Home Page", currentRoute: req.url });
+  res.render("home", {
+    title: "Home",
+    data: "Welcome to the Home Page",
+    currentRoute: req.url,
+    noLayout: true,
+  });
 });
 
 app.get("/restaurants", (req, res) => {
-  res.render("restaurants", { title: "About", data: "About Us Page", currentRoute: req.url });
+  res.render("restaurants", {
+    title: "About",
+    data: "About Us Page",
+    currentRoute: req.url,
+    noLayout: true,
+  });
+});
+app.get("/login", (req, res) => {
+  res.render("login", {
+    title: "About",
+    data: "About Us Page",
+    currentRoute: req.url,
+    noLayout: false,
+  });
 });
 
 app.listen(PORT, () => {
